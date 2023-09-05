6fc6ba917df3cdce6e589670bbf1c848657789a2
GabrielLopes03 <138938557+GabrielLopes03@users.noreply.github.com>
2023-09-05 19:46:11 -0300

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -4,4 +4,41 @@ document.getElementById('botaoEntrar')
             e.preventDefault();
         })
 
-        
+function verificar(){
+    //Pegando o s valores do input email e senha
+    let email = document.getElementById('username').value;
+    let senha = document.getElementById('password').value;
+
+    if(email == "" || senha == ""){
+        alert("Obrigatorio o preenchimento de todos os campos")
+        document.getElementById('username').focus();
+    } else{
+        consultar(email,senha);
+    };
+
+};
+
+function consultar(email,senha){
+    let usuarios = new Array();//Aqui estamos definindo uma matriz
+
+    //Verificando se a chave usurios existe no local
+    if(localStorage.hasOwmProperty('usuarios')){
+        //Recuperar os valores da propriedades usuarios do localStorge
+        //Convertendo o USUARIO para objeto
+
+        usuarios = JSON.parse(localStorage.get('usuarios'))
+
+    }
+
+    for(let i = 0;i<usuarios.length;i++){
+
+        if(usuarios[i].email == email && usuario[i].senha == senha){
+            window.open("../assets/index.html")
+            window.close();
+            break;
+        }
+
+    }
+}
+alert("email ou senha inválido");
+document.getElementById('username').focus()
