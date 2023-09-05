c12899f67fb6ec4e51c3275d0cd92e99bcc1f770
GabrielLopes03 <138938557+GabrielLopes03@users.noreply.github.com>
2023-09-05 19:09:52 -0300

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,20 +1,7 @@
-function verifica() {
-    /* Pegando os valores do email e senha */
-    let email = document.querySelector("#username").value;
-    let senha = document.getElementById('password').value;
+/* Criando evento botão */
+document.getElementById('botaoEntrar')
+        .addEventListener ('click',(e) =>{
+            e.preventDefault();
+        })
 
-    if (email == "" || senha == "") {
-        alert("Preencha todos os campos")
-        document.querySelector("#username").focus()
-    } else {
-        pesquisa(email, senha)
-    }
-}
-
-function pesquisa(email, senha) {
-
-    if (email == "gsbriel.a.lopes@gmail.com" && senha == 12345678) {
-
-        window.open("index.html")
-    }
-}
+        
