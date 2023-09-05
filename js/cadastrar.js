f33814315316c727ea3804dae2e8b269d63db8cc
GabrielLopes03 <138938557+GabrielLopes03@users.noreply.github.com>
2023-09-05 19:09:29 -0300

    Add files via upload

---
+++ js/cadastrar.js
@@ -0,0 +1,9 @@
+const email = document.getElementById('emailUs');
+const emailC = document.getElementById('emailCad');
+
+
+    function criaAcc(){
+        if(email !== ${'emailCad'}){
+            document.addEventListener
+        }
+    }
--- js/cep.js
+++ js/cep.js
@@ -1,37 +1,33 @@
-/* ARROW FUNCTION */
+//arrow function
 const pesquisaCEP = async () => {
     const cep = document.getElementById('cep').value;
 
-    // passando o cep para API viacep 
-    // usando template script
+    //passando o cep para API viacep
+    //usando template script ``
     const url = `http://viacep.com.br/ws/${cep}/json/`;
-}
 
-/* Função preencher fortmulario */
+    const dados = await fetch(url);
+    const endereco = await dados.json();
+
+    if (endereco.erro) {
+        alert(`CEP ${cep} inválido`);
+        document.getElementById('cep').value = '';
+        document.getElementById('cep').focus();
+    } else {
+        preencherFormulario(endereco);
+    }
+}
+//Funcao para preencher o formulário
 
 const preencherFormulario = (endereco) => {
-    document.getElementById('rua').value = endereco.lenght
-    document.getElementById('bairro').value = endereco.lenght
-    document.getElementById('cidade').value = endereco.lenght
-    document.getElementById('uf').value = endereco.uf
-}
 
+    document.getElementById('rua').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('uf').value = endereco.uf;
+
+
+}
 /* Criando um evento do input CEP */
 document.getElementById('cep')
     .addEventListener('focusout', pesquisaCEP);
-
-/* Criando CEP indefinido */
-const cep = document.getElementById('cepCa').value
-
-function cepInvalido() {
-    if (cep !== 13046 - 510) {
-        document.getElementById('nome').value = "undefined";
-        document.getElementById('cepCa').value = "";
-        document.getElementById('rua').value = "undefined";
-        document.getElementById('bairro').value = "undefined";
-        document.getElementById('cidade').value = "undefined";
-        document.getElementById('uf').value = "undefined";
-        document.getElementById('email').value = "undefined";
-        document.getElementById('cepCa').focus();
-    }
-}
